ef302069f2282126403e6d64e0269c636df89be1
1923scasrich <1923scasrich@10.0.0.27>
2019-09-09 23:06:57 -0400

    This program is ready for review.

---
+++ TenLinesProject/src/TenLines/TenLines.java
@@ -0,0 +1,21 @@
+package TenLines;
+
+public class TenLines {
+
+	/*
+	 * Program name: Ten Lines Project
+	 * Purpose: To print 10 lines of code
+	 * */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("A lot has happened in 2019.");
+		System.out.print("Milo Murphy\'s Law aired its second season,\nSMOSH is now owned by Rhett and Link,\n");
+		System.out.print("Epic Rap Battles of History started its sixth season,\nThe web series Meta Runner is availible on YouTube,\n");
+		System.out.print("I graduated high school and started college,\nI\'m making memes on YouTube,\n");
+		System.out.println("I can make my own music with MAGIX Music Maker and GarageBand,\nand I now have Garry's Mod.");
+		System.out.println("Man, what a year.");
+	}
+
+}
